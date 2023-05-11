68624c4fe388f176e378e9a8b40dbbc9ec178200
lai2-01 <lai2-01@itismeucci.com>
2023-05-11 12:39:06 +0200

    primo commit

---
+++ src/ambulatorio.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ambulatorio {
+    ArrayList<Animale> animali = new ArrayList<>();
+    private int maxGatti; 
+    private int maxCani;
+    
+    public ambulatorio(int maxGatti, int maxCani) {
+        this.maxGatti = maxGatti;
+        this.maxCani = maxCani;
+    }
+   
+    
+
+    public void entra(){
+            
+    }
+    public void esce(){
+            
+    }
+}
